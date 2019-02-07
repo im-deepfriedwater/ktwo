bd4a75df5ee94a388d2382800db030fb422aaaf9
Alejandro Zapata <azapataa@lion.lmu.edu>
2019-02-07 04:23:13 -0800

    integrated prototype architect controller with correct assets

---
+++ unity-ktwo/Assets/Scripts/Input/InputManager.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public enum Buttons {
+	Right,
+	Left,
+	Up,
+	Down,
+	A,
+	B,
+    Click
+}
+
+public enum Condition
+{
+	GreaterThan,
+	LessThan
+}
+
+// Is responsible for telling behaviours if a certain button is pressed.
+[System.Serializable]
+public class InputAxisState
+{
+	public string axisName;
+	public float offValue;
+	public Buttons button;
+	public Condition condition;
+
+	public bool value
+    {
+		get
+        {
+			var val = Input.GetAxis(axisName);
+
+			switch (condition)
+            {
+				case Condition.GreaterThan:
+					return val > offValue;
+				case Condition.LessThan:
+					return val < offValue;
+			}
+
+			return false;
+		}
+	}
+}
+
+public class InputManager : MonoBehaviour
+{
+	public InputAxisState[] inputs;
+	public InputState playerInputState;
+
+	// Update is called once per frame
+	void Update ()
+    {
+		foreach (var input in inputs)
+        {
+			playerInputState.SetButtonValue(input.button, input.value);
+		}
+	}
+}
