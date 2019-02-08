7b2b48d66308433df45b07e1552c2a5e9b073c65
Alejandro Zapata <azapataa@lion.lmu.edu>
2019-02-07 16:00:53 -0800

    removed strafing from movement script

--- unity-ktwo/Assets/Invector-3rdPersonController/Basic Locomotion LITE/Scripts/CharacterController/vThirdPersonAnimator.cs
+++ unity-ktwo/Assets/Invector-3rdPersonController/Basic Locomotion LITE/Scripts/CharacterController/vThirdPersonAnimator.cs
@@ -9,18 +9,11 @@ namespace Invector.CharacterController
         {
             if (animator == null || !animator.enabled) return;
 
-            animator.SetBool("IsStrafing", isStrafing);
             animator.SetBool("IsGrounded", isGrounded);
             animator.SetFloat("GroundDistance", groundDistance);
 
             if (!isGrounded)
                 animator.SetFloat("VerticalVelocity", verticalVelocity);
-
-            if (isStrafing)
-            {
-                // strafe movement get the input 1 or -1
-                animator.SetFloat("InputHorizontal", direction, 0.1f, Time.deltaTime);
-            }
 
             // fre movement get the input 0 to 1
             animator.SetFloat("InputVertical", speed, 0.1f, Time.deltaTime);
@@ -36,28 +29,13 @@ namespace Invector.CharacterController
 
                 var speedDir = Mathf.Abs(direction) + Mathf.Abs(speed);
                 speedDir = Mathf.Clamp(speedDir, 0, 1);
-                var strafeSpeed = (isSprinting ? 1.5f : 1f) * Mathf.Clamp(speedDir, 0f, 1f);
                 
-                // strafe extra speed
-                if (isStrafing)
-                {
-                    if (strafeSpeed <= 0.5f)
-                        ControlSpeed(strafeWalkSpeed);
-                    else if (strafeSpeed > 0.5f && strafeSpeed <= 1f)
-                        ControlSpeed(strafeRunningSpeed);
-                    else
-                        ControlSpeed(strafeSprintSpeed);
-                }
-                else if (!isStrafing)
-                {
-                    // free extra speed                
-                    if (speed <= 0.5f)
-                        ControlSpeed(freeWalkSpeed);
-                    else if (speed > 0.5 && speed <= 1f)
-                        ControlSpeed(freeRunningSpeed);
-                    else
-                        ControlSpeed(freeSprintSpeed);
-                }
+                if (speed <= 0.5f)
+                    ControlSpeed(freeWalkSpeed);
+                else if (speed > 0.5 && speed <= 1f)
+                    ControlSpeed(freeRunningSpeed);
+                else
+                    ControlSpeed(freeSprintSpeed);
             }
         }
     }
