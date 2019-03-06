abcb13647d411e2c690331494f4893a46b258664
Justin Torres <jtorre39@lion.lmu.edu>
2019-03-06 00:37:58 -0800

    Starting networking.

---
+++ unity-ktwo/Assets/Scripts/Networking/NetworkManagerHost.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class NetworkManagerHost : MonoBehaviour
+{
+    NetworkManager manager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        manager = GetComponent<NetworkManager>();
+        if (!manager.StartServer())
+        {
+            Debug.Log("Server failed to start.");
+        }
+    }
+}
---
+++ unity-ktwo/Assets/Scripts/Networking/PlayerConnectionObject.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+// A virtual object meant to represent a client that has connected.
+// We control state of each client here and have to be careful to
+// make sure each client loads the same scenes and is in the same states.
+public class PlayerConnectionObject : NetworkBehaviour
+{
+    bool isPartyLeader = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!isLocalPlayer)
+        {
+            return; // This belongs to a different player.
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
