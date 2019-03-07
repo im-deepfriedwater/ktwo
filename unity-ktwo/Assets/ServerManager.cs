ecb7f497429992f66413f46c27bf862ce8c2137c
Justin Torres <jtorre39@lion.lmu.edu>
2019-03-06 18:36:49 -0800

    Added Server starter

---
+++ unity-ktwo/Assets/ServerManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class ServerManager : MonoBehaviour
+{
+    NetworkManagerHost managerHost;
+    // Start is called before the first frame update
+    void Start()
+    {
+        managerHost = GetComponent<NetworkManagerHost>();
+        managerHost.Start();
+        Debug.Log("Server started");
+    }
+}
