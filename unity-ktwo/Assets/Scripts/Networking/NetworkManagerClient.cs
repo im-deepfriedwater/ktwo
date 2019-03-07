c20b63f2a8f277eb8c001f0f156e7b416be96220
Justin Torres <jtorre39@lion.lmu.edu>
2019-03-06 17:35:31 -0800

    WIP client and server

---
+++ unity-ktwo/Assets/Scripts/Networking/NetworkManagerClient.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class NetworkManagerClient : MonoBehaviour
+{
+    NetworkManager manager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        manager.StartClient();
+    }
+}
