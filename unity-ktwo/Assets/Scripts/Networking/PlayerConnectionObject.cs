31dec2030055f9ad7ef90c22405aa89bf77e40d1
it-door <tedchu4@gmail.com>
2019-03-06 22:27:12 -0800

    Added rpc script to PlayerConnectionObjects

--- unity-ktwo/Assets/Scripts/Networking/PlayerConnectionObject.cs
+++ unity-ktwo/Assets/Scripts/Networking/PlayerConnectionObject.cs
@@ -2,12 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
+using UnityEngine.SceneManagement;
 
 // A virtual object meant to represent a client that has connected.
 // We control state of each client here and have to be careful to
 // make sure each client loads the same scenes and is in the same states.
 public class PlayerConnectionObject : NetworkBehaviour
 {
+    [ClientRpc]
+    public void RpcLoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
+    }
     bool isPartyLeader = false;
 
     // Start is called before the first frame update
