9837665a36025e1f9a3c3ac47f2b43c0397425cb
Justin Torres <jtorre39@lion.lmu.edu>
2019-03-21 23:21:51 -0700

    Adding Unity made navmesh components

---
+++ unity-ktwo/Assets/Scripts/ExternalScripts/Invector-3rdPersonController/Basic Locomotion LITE/Scripts/CharacterController/vThirdPersonAnimator.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Invector.CharacterController
+{
+    public abstract class vThirdPersonAnimator : vThirdPersonMotor
+    {
+        public virtual void UpdateAnimator()
+        {
+            if (animator == null || !animator.enabled) return;
+
+            animator.SetBool("IsGrounded", isGrounded);
+            animator.SetFloat("GroundDistance", groundDistance);
+
+            if (!isGrounded)
+                animator.SetFloat("VerticalVelocity", verticalVelocity);
+
+            // fre movement get the input 0 to 1
+            animator.SetFloat("InputVertical", speed, 0.1f, Time.deltaTime);
+        }
+
+        public void OnAnimatorMove()
+        {
+            // we implement this function to override the default root motion.
+            // this allows us to modify the positional speed before it's applied.
+            if (isGrounded)
+            {
+                transform.rotation = animator.rootRotation;
+
+                var speedDir = Mathf.Abs(direction) + Mathf.Abs(speed);
+                speedDir = Mathf.Clamp(speedDir, 0, 1);
+                
+                if (speed <= 0.5f)
+                    ControlSpeed(freeWalkSpeed);
+                else if (speed > 0.5 && speed <= 1f)
+                    ControlSpeed(freeRunningSpeed);
+                else
+                    ControlSpeed(freeSprintSpeed);
+            }
+        }
+    }
+}
