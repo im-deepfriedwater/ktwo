9445334321e72b757f67e463b4c61a4cd3fb05fa
Alejandro Zapata <azapataa@lion.lmu.edu>
2019-04-03 22:38:24 -0700

    beginnings of chef prototype and prefabs

---
+++ unity-ktwo/Assets/Scripts/Behaviours/GreasePuddleBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GreasePuddleBehaviour : BasePuddleBehaviour
+{
+    public int knockbackForce;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (CannotBeUsed(other.gameObject)) return;
+
+        if (other.gameObject.tag != "Zombie") return;
+        affectedEntities.Add(other.gameObject);
+
+        // knock zombies back
+
+        // StartCoroutine(
+        //     RemoveFromHashSet(other.gameObject, duration)
+        // );
+        numberOfUses -= 1;
+    }
+}
