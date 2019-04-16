c1c69ae879e676cc26aaca6ed7b513b149ed3088
Justin Torres <jtorre39@lion.lmu.edu>
2019-04-16 13:39:01 -0700

    character rotation put in

---
+++ unity-ktwo/Assets/CSSRotate.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CSSRotate : MonoBehaviour
+{
+    public float rotationSpeed = -1f;
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(
+            transform.rotation.x, 
+            transform.rotation.y + rotationSpeed, 
+            transform.rotation.z
+        );
+    }
+}
