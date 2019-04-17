1c6c2360a22185320854001d420b9084e2005e59
Justin Torres <jtorre39@lion.lmu.edu>
2019-04-17 01:00:33 -0700

    reorganized textures, filled in architect in roster data

--- unity-ktwo/Assets/Scripts/UI/CharacterInfoStruct.cs
+++ unity-ktwo/Assets/Scripts/UI/CharacterInfoStruct.cs
@@ -8,12 +8,14 @@ public struct CharacterInfoStruct
 {
     public CharacterEnum character;
 
+    [TextArea(4, 4)]
     public string characterDescription;
 
-   public  GameObject previewObject;
+    public GameObject previewObject;
 
+    [TextArea(4, 4)]
     // These both should have a 1-to-1
     // correspondence and be length 4.
     public string[] abilityDescriptions;
-    public Image[] abilityIcons;
+    public Texture[] abilityIcons;
 }
