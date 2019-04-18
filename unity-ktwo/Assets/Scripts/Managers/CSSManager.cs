d1b9542a5f25db493af0e408085d333fcceb11e7
Justin Torres <jtorre39@lion.lmu.edu>
2019-04-18 02:12:19 -0700

    Data structures and systems in place, preview, and choose working.

--- unity-ktwo/Assets/Scripts/Managers/CSSManager.cs
+++ unity-ktwo/Assets/Scripts/Managers/CSSManager.cs
@@ -1,12 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
+// The lists are populated in the inspector
+// and *should* be in the character order
+// as defined in the CharacterEnum.
+
+// This manager depends on having the 
+// CSS prefab in the scene under
+// the canvas.
 public class CSSManager : MonoBehaviour
 {
     public static CSSManager instance;
-    CharacterEnum chosenCharacter;
-    public List<CharacterInfoStruct> rosterData;
+
+    public List<GameObject> previewModels;
+
+    public List<GameObject> abilityIcons;
+    CharacterEnum? chosenCharacter = null;
+    public List<CSSCharacterInfoStruct> rosterData;
+
+    public Text textDescription;
+    public Text name;
+    public GameObject currentModel = null;
+
+    string currentLoadedText;
 
     // Start is called before the first frame update
     void Start()
@@ -14,17 +32,60 @@ public class CSSManager : MonoBehaviour
         instance = this;
     }
 
-    public void PreviewCharacter(CharacterEnum characterToPreview)
+    public void PreviewCharacter(CharacterEnum? characterToPreview)
     {
-        throw new System.NotImplementedException();
+        if (characterToPreview == null) return;
+        var characterIndex = (int)characterToPreview;
+        UpdateAbilityIcons(characterIndex);
+        UpdateCharacterDescription(characterIndex);
+        UpdateCharacterTitle(characterIndex);
+        UpdatePreviewModel(characterIndex);
     }
 
     public void RestoreChosenCharacter()
     {
-        throw new System.NotImplementedException();
+        // if currentModel is null, we don't have a character to restore.
+        if (!currentModel) return;
+        PreviewCharacter(chosenCharacter);
     }
 
-    public void ChooseCharacter(CharacterEnum characterToChoose)
+    public void ChooseCharacter(CharacterEnum character)
+    {
+        chosenCharacter = character;
+    }
+
+    void UpdateAbilityIcons(int characterId)
+    {
+        for (int i = 0; i < abilityIcons.Count; i++)
+        {
+            var iconToChange = abilityIcons[i].GetComponent<Image>();
+            iconToChange.sprite = rosterData[characterId].abilityIcons[i];
+        }    
+    }
+    void UpdateCharacterTitle(int characterId)
+    {
+        name.text = string.Format("\"The {0}\"", rosterData[characterId].character.ToString());
+    }
+
+    void UpdateCharacterDescription(int characterId)
+    {
+        textDescription.text = rosterData[characterId].characterDescription;
+    }
+
+    void UpdatePreviewModel(int characterId)
+    {
+        if (currentModel == rosterData[characterId].previewModel) return;
+
+        if (currentModel)
+        {
+            currentModel.SetActive(false);
+        }
+
+        currentModel = rosterData[characterId].previewModel;
+        currentModel.SetActive(true);
+    }
+
+    void ShowAbilityDescription(int abilityIndex)
     {
         throw new System.NotImplementedException();
     }
--- unity-ktwo/Assets/Scripts/UI/CSSCharacterInfoStruct.cs
+++ unity-ktwo/Assets/Scripts/UI/CSSCharacterInfoStruct.cs
@@ -4,18 +4,18 @@ using UnityEngine;
 using UnityEngine.UI;
 
 [System.Serializable]
-public struct CharacterInfoStruct
+public struct CSSCharacterInfoStruct
 {
     public CharacterEnum character;
 
     [TextArea(4, 4)]
     public string characterDescription;
 
-    public GameObject previewObject;
+    public GameObject previewModel;
 
     [TextArea(4, 4)]
     // These both should have a 1-to-1
     // correspondence and be length 4.
     public string[] abilityDescriptions;
-    public Texture[] abilityIcons;
+    public Sprite[] abilityIcons;
 }
