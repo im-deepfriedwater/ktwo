2a9fc3b2e96c1b57efaa60e528b65301cfe90b1b
Alejandro Zapata <azapataa@lion.lmu.edu>
2019-04-22 19:35:38 -0700

    working food heal abilty

--- unity-ktwo/Assets/Scripts/Behaviours/FoodBehaviour.cs
+++ unity-ktwo/Assets/Scripts/Behaviours/FoodBehaviour.cs
@@ -1,14 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
-public class FoodBehaviour : MonoBehaviour
+public class FoodBehaviour : NetworkBehaviour
 {
     [Header("Food Settings")]
     public int healAmount;
 
     void OnTriggerEnter(Collider other)
     {
+        if (!isServer) return;
         if (other.gameObject.tag != "Player") return;
         other.gameObject.GetComponent<DamagablePlayer>().Heal(healAmount);
         Destroy(gameObject);
