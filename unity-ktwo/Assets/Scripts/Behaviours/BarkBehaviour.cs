43b417372362a0da4557859d1340416a0e60c9f1
Alejandro Zapata <azapataa@lion.lmu.edu>
2019-04-30 14:06:27 -0700

    WIP pet ability

---
+++ unity-ktwo/Assets/Scripts/Behaviours/BarkBehaviour.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class BarkBehaviour : NetworkBehaviour
+{
+    public float duration;
+
+    Animator animator;
+
+    void Start()
+    {
+        animator = GetComponentInParent<Animator>();
+        animator.SetBool("IsBarking", true);
+        StartCoroutine(
+            ResetBarking()
+        );
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        if (!isServer) return;
+        if (other.gameObject.tag != "Zombie") return;
+        other.GetComponent<DamagableEnemy>().Hit(0f, -other.transform.forward, true);
+    }
+
+    private IEnumerator ResetBarking()
+    {
+        yield return new WaitForSeconds(duration);
+        animator.SetBool("IsBarking", false);
+        Destroy(gameObject);
+    }
+}
---
+++ unity-ktwo/Assets/Scripts/Behaviours/PetBehaviour.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PetBehaviour : NetworkBehaviour
+{
+    public float healAmount;
+    public float speedBuffPercent;
+    public float speedBuffDuration;
+
+    Animator animator;
+    ParticleSystem heart;
+
+    void Start()
+    {
+        heart = GetComponent<ParticleSystem>();
+        heart.Play();
+        StartCoroutine(
+            EndPet()
+        );
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (!isServer) return;
+        if (other.gameObject.tag != "Player") return;
+        other.GetComponent<DamagablePlayer>().Heal(healAmount);
+    }
+
+    private IEnumerator EndPet()
+    {
+        yield return new WaitForSeconds(1);
+        Destroy(gameObject);
+    }
+}
