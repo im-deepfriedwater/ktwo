9a9b2037e8a009872b1d843f4a91b8ea3000ed1f
Alejandro Zapata <azapataa@lion.lmu.edu>
2019-04-29 22:06:36 -0700

    semi-working herder bite

---
+++ unity-ktwo/Assets/Scripts/Behaviours/BiteBehaviour.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class BiteBehaviour : NetworkBehaviour
+{
+    public int damageAmount;
+    public float duration;
+
+    Animator animator;
+
+    void Start()
+    {
+        animator = GetComponentInParent<Animator>();
+        animator.SetBool("IsBiting", true);
+        StartCoroutine(
+            ResetBiting()
+        );
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (!isServer) return;
+        if (other.gameObject.tag != "Zombie") return;
+        other.GetComponent<DamagableEnemy>().Hit(damageAmount, gameObject.transform.forward, true);
+    }
+
+    private IEnumerator ResetBiting()
+    {
+        yield return new WaitForSeconds(duration);
+        animator.SetBool("IsBiting", false);
+        Destroy(gameObject);
+    }
+}
